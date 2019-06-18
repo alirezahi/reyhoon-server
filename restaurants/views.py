1f66110ad9aadfd0f9d18b4f8f6263d715533946
Alireza Heydari <alireza97hi@gmail.com>
2019-06-19 02:08:06 +0430

    add areas list to view

--- restaurants/views.py
+++ restaurants/views.py
@@ -6,6 +6,22 @@ from .serializers import *
 # Create your views here.
 
 def restaurants_list(request):
-    restaurants = Restaurant.objects.all()
-    restaurants_list = RestaurantSerializer(restaurants, many=True)
-    return JsonResponse(restaurants_list.data,safe=False)
+    if request.method == 'GET':
+        params = request.GET
+        params = [
+            ('area', params.get('area', None)),
+            ('categories__name', params.get('category', None)),
+        ]
+        filters = dict( (key, value) for (key, value) in params if value != None )
+        restaurants = Restaurant.objects.filter(**filters)
+        restaurants_list = RestaurantSerializer(restaurants, many=True)
+        return JsonResponse(restaurants_list.data,safe=False)
+
+
+def area_list(request):
+    if request.method == 'GET':
+        params = request.GET
+        area = params.get('area', '')
+        areas = Address.objects.filter(area__icontains=area).values('area')
+        areas_list = AreaSerializer(areas, many=True)
+        return JsonResponse(areas_list.data,safe=False)
